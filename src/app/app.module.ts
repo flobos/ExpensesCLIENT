9ab611f122a1cbfd5775634c2c9ad67cc8de10ae
Fernando Lobos <fernandolobos@gmail.com>
2019-12-18 12:26:31 -0300

    Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EntriesComponent } from './entries/entries.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+
+//services
+
+import { EntryService } from './entry.service';
+import { AppRouterModule } from './app-router.module';
+import { HttpClientModule } from '@angular/common/http';
+
+//Material
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatInputModule,MatCardModule,MatSelectModule,MatTableModule } from '@angular/material';
+import { NewEntryComponent } from './new-entry/new-entry.component';
+
+
+//forms
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntriesComponent,
+    FooterComponent,
+    HeaderComponent,
+    NewEntryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule, MatInputModule,MatCardModule,MatSelectModule,    
+    AppRouterModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [EntryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
